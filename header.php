0f55a1708fd14e6ff475cad7ce581de0f021e3a7
MHervian <muaxmad@gmail.com>
2020-01-15 13:36:12 +0700

    Update meta viewport

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     
     <!-- Place favicon.ico in the root directory -->
     <link rel="apple-touch-icon" href="<?php bloginfo( 'template_url' ); ?>/images/HappyLife-putih.png">
