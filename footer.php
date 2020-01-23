3e7e1ed9c48ca3ec5bad6a61a79c102a6ca92a60
MHervian <muaxmad@gmail.com>
2020-01-23 14:05:24 +0700

    Fixing post/article bug

--- footer.php
+++ footer.php
@@ -76,7 +76,7 @@
         <div class="footer-bottom">
             <div class="container">
                 <p class="copyright">
-                    &copy;2019 - <?php echo date('Y'); ?> All rights reserved
+                    &copy;2019 - <?php echo date('Y'); ?> PT Solusi Hidup Bahagia, all rights reserved.
                 </p>
             </div>
         </div>
