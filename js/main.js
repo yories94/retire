c55bad2ad87ff84b9d6d0235bd9b422499238d61
MHervian <mhervian0@gmail.com>
2020-03-18 06:49:55 +0700

    Update homepage happylife

--- js/main.js
+++ js/main.js
@@ -6,4 +6,15 @@
     $(window).on("load", function () {
         $('.preloader').fadeOut(1000);
     });
+
+    // Activate carousel for display categories carousel
+    $(".slider").owlCarousel({
+        items: 3,
+        autoPlay: 4000,
+        responsive: true,
+        itemsMobile: [479, 2],
+        itemsTablet: [768, 3],
+        itemsDesktopSmall: [991, 4]
+    });
+    
 })(jQuery);
